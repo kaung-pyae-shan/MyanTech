36a90f949b8d7f3b17bfcab7f57b6821a1721baf
kaungpyaeshan <kaung.p.shan@gmail.com>
2025-02-09 10:27:26 +0630

    Initial commit

---
+++ MyanTechAPI/src/main/java/com/byteriders/myantech/MyanTechApiApplication.java
@@ -0,0 +1,13 @@
+package com.byteriders.myantech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyanTechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyanTechApiApplication.class, args);
+	}
+
+}
---
+++ MyanTechAPI/src/test/java/com/byteriders/myantech/MyanTechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byteriders.myantech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyanTechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
