eed55b55375ec02817c307c032f6e3f4e9d75b97
kaungpyaeshan <kaung.p.shan@gmail.com>
2025-02-11 09:50:19 +0630

    Add Order Creation Form

--- MyanTechAPI/src/main/java/com/byteriders/myantech/model/entity/Shop.java
+++ MyanTechAPI/src/main/java/com/byteriders/myantech/model/entity/Shop.java
@@ -1,7 +1,5 @@
 package com.byteriders.myantech.model.entity;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
