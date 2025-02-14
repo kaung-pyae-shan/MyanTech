20f0cf1e60732acfa2b221d9d571308201bc46dc
yamone178 <yamonemyatmoe1592@gmail.com>
2025-02-15 01:25:21 +0630

    test

--- MyanTechAPI/src/main/java/com/byteriders/myantech/model/service/impl/AssignTruckServiceImpl.java
+++ MyanTechAPI/src/main/java/com/byteriders/myantech/model/service/impl/AssignTruckServiceImpl.java
@@ -13,6 +13,7 @@ import com.byteriders.myantech.model.dto.output.Response;
 import com.byteriders.myantech.model.entity.AssignTruck;
 import com.byteriders.myantech.model.entity.Driver;
 import com.byteriders.myantech.model.entity.Order;
+import com.byteriders.myantech.model.entity.Order.Status;
 import com.byteriders.myantech.model.enums.AssignTruckStatus;
 import com.byteriders.myantech.model.exception.NotFoundException;
 import com.byteriders.myantech.model.repo.AssignTruckRepo;
@@ -83,8 +84,8 @@ public class AssignTruckServiceImpl implements AssignTruckService {
 		        throw new NotFoundException("AssignTruck with invoiceNo: " + order.getInvoiceNo() + " already exists.");
 		    }
 			
-//			order.setStatus(Status.DELIVERED);
-//			orderRepo.save(order);
+		//order.setStatus(Status.DELIVERING);
+		//	orderRepo.save(order);
 			
 			AssignTruck assignTruck = AssignTruck.builder()
 		                .order(order)
@@ -286,7 +287,8 @@ public class AssignTruckServiceImpl implements AssignTruckService {
 	        orderDTO.setDriver_name(assignTruck.getDriver().getDriverName());
 	        orderDTO.setVehicle_plate_no(assignTruck.getDriver().getVehiclePlateNo());
 	        orderDTO.setDelivery_status(assignTruck.getStatus() != null ? assignTruck.getStatus().toString() : "Unknown");
-
+	        orderDTO.setOrder_status(order.getStatus().toString());
+	        
 	        return orderDTO;
 	    }).collect(Collectors.toList());
 
